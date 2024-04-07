8010279e0982f9bc4838fbe3dd8a17022a1e1902
Maryna Kamienieva <mkdirwd@gmail.com>
2024-04-07 13:42:52 +0200

    add method readfile

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 
+def readfile(file):
+    data = list()
+    with open(file, 'r') as files:
+        for line in files:
+            data.append(line.strip().split(','))
+    return data
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def main():
+    training_data = input("Please, provide path to file with training data");
+    test_data = input("Please, provide path to file with test data");
+    learning_rate = input("Please, provide learning rate");
+    epochs = input("Please, provide number of epochs");
+
+
+    menu = {}
+    menu['1'] = "Enter new observations"
+    menu['2'] = "Exiting the program"
+
+    while True:
+        options = sorted(menu.keys())
+
+        for enter in options:
+            print(enter, menu[enter])
+        choose = input("Please, choose an option: ")
+
+        if choose == '1':
+        elif choose == '2':
+            break
+
+        else:
+            print("Unknown option was chosen!")
+
+
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
